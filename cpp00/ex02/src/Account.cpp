f8f68d82075ff4d17b84ade71219979ee1c14b09
fbock <fbock@student.42heilbronn.de>
2023-12-13 15:47:12 +0100

    alias commit

---
+++ cpp00/ex02/src/Account.cpp
@@ -0,0 +1,71 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Account.cpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ***REMOVED*** <***REMOVED***@student.***REMOVED***.de>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/13 15:27:07 by ***REMOVED***             #+#    #+#             */
+/*   Updated: 2023/12/13 15:46:57 by ***REMOVED***            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Account.hpp"
+
+int	Account::getNbAccounts(void)
+{
+
+}
+
+int	Account::getTotalAmount(void)
+{
+
+}
+
+int	Account::getNbDeposits(void)
+{
+
+}
+
+int	Account::getNbWithdrawals(void)
+{
+
+}
+
+void	Account::displayAccountsInfos(void)
+{
+
+}
+
+Account::Account(int initial_deposit)
+{
+
+}
+
+Account::~Account(void)
+{
+
+}
+
+void	Account::makeDeposit(int deposit)
+{
+
+}
+
+bool	Account::makeWithdrawal(int withdrawl)
+{
+
+}
+
+int	Account::checkAmount(void) const
+{
+
+}
+
+void	Account::displayStatus(void) const
+{
+
+}
+
+
+
