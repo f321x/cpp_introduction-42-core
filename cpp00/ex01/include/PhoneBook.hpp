6e3fa1e70964582835add9feab903a21fd30b4eb
fbock <fbock@student.42heilbronn.de>
2023-12-13 15:10:45 +0100

    alias commit

--- cpp00/ex01/include/PhoneBook.hpp
+++ cpp00/ex01/include/PhoneBook.hpp
@@ -6,13 +6,14 @@
 /*   By: ***REMOVED*** <***REMOVED***@student.***REMOVED***.de>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/12 15:43:23 by ***REMOVED***             #+#    #+#             */
-/*   Updated: 2023/12/13 13:19:55 by ***REMOVED***            ###   ########.fr       */
+/*   Updated: 2023/12/13 15:09:17 by ***REMOVED***            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
 
 #include <iostream>
+#include <iomanip>
 #include "Contact.hpp"
 
 class PhoneBook {
@@ -24,6 +25,9 @@ class PhoneBook {
 	private:
 		Contact _contacts[8];
 		uint8_t	_current_contact;
+		bool	_contacts_filled;
 
 		std::string	_get_input(void);
+		std::string	_trunc(std::string field);
+		void		_print_contact(uint8_t contact_index);
 };
--- cpp00/ex01/src/PhoneBook.cpp
+++ cpp00/ex01/src/PhoneBook.cpp
@@ -6,13 +6,13 @@
 /*   By: ***REMOVED*** <***REMOVED***@student.***REMOVED***.de>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/12 15:18:27 by ***REMOVED***             #+#    #+#             */
-/*   Updated: 2023/12/13 13:33:43 by ***REMOVED***            ###   ########.fr       */
+/*   Updated: 2023/12/13 15:08:43 by ***REMOVED***            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "PhoneBook.hpp"
 
-PhoneBook::PhoneBook(void) : _current_contact(0) {}
+PhoneBook::PhoneBook(void) : _current_contact(0), _contacts_filled(false) {}
 
 std::string PhoneBook::_get_input(void)
 {
@@ -44,12 +44,63 @@ void	PhoneBook::add(void)
 	_contacts[_current_contact].darkest_secret = _get_input();
 	std::cout << "Contact successfully saved!" << std::endl;
 	if (_current_contact >= 7)
+	{
 		_current_contact = 0;
+		_contacts_filled = true;
+	}
 	else
 		_current_contact++;
 }
 
 void	PhoneBook::search(void)
 {
+	uint8_t		max_index;
+	std::string	user_input;
 
+	max_index = _current_contact;
+	if (_contacts_filled)
+		max_index = 8;
+	std::cout << std::endl << "|" << std::setw(10) << "INDEX" << "|" << std::setw(10)
+		<< "FIRST NAME" << "|" << std::setw(10) << "LAST NAME" << "|"
+		<< std::setw(10) << "NICKNAME" << "|" << std::endl;
+	std::cout << "---------------------------------------------" << std::endl;
+	for (uint8_t i = 0; i < max_index; i++)
+	{
+		std::cout << "|" << std::setw(10) << std::to_string(i);
+        std::cout << "|" << std::setw(10) << _trunc(_contacts[i].first_name);
+        std::cout << "|" << std::setw(10) << _trunc(_contacts[i].last_name);
+        std::cout << "|" << std::setw(10) << _trunc(_contacts[i].nickname);
+        std::cout << "|" << std::endl;
+	}
+	std::cout << std::endl;
+	if (max_index < 1)
+		return ;
+	std::cout << "Enter contact index: ";
+	user_input = _get_input();
+	while (user_input.length() > 1 || !std::isdigit(user_input[0]) || std::stoll(user_input) < 0
+			|| std::stoll(user_input) >= max_index)
+	{
+		std::cout << "Non-existing index, enter index between 0 and "
+		<< std::to_string(max_index - 1) << ": ";
+		user_input = _get_input();
+	}
+	_print_contact(std::stoi(user_input));
+}
+
+std::string	PhoneBook::_trunc(std::string field)
+{
+	if (field.length() > 10)
+		field = field.substr(0, 9) + ".";
+	return (field);
+}
+
+void	PhoneBook::_print_contact(uint8_t	contact_index)
+{
+	std::cout << std::endl;
+	std::cout << std::setw(17) << "First name: " << _contacts[contact_index].first_name << std::endl;
+	std::cout << std::setw(17) << "Last name: " << _contacts[contact_index].last_name << std::endl;
+	std::cout << std::setw(17) << "Nickname: " << _contacts[contact_index].nickname << std::endl;
+	std::cout << std::setw(17) << "Phone number: " << _contacts[contact_index].phone_nbr << std::endl;
+	std::cout << std::setw(17) << "Darkest secret: " << _contacts[contact_index].darkest_secret << std::endl;
+	std::cout << std::endl;
 }
