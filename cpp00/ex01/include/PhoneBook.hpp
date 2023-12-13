e645412d16f9feadea57edb9dd014e29b7d574a8
fbock <fbock@student.42heilbronn.de>
2023-12-13 15:10:45 +0100

    alias commit

--- cpp00/ex01/include/PhoneBook.hpp
+++ cpp00/ex01/include/PhoneBook.hpp
@@ -6,13 +6,14 @@
 /*   By: fbock <fbock@student.42heilbronn.de>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/12 15:43:23 by fbock             #+#    #+#             */
-/*   Updated: 2023/12/13 13:19:55 by fbock            ###   ########.fr       */
+/*   Updated: 2023/12/13 15:09:17 by fbock            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
 
 #include <iostream>
+#include <iomanip>
 #include "Contact.hpp"
 
 class PhoneBook {
@@ -24,6 +25,9 @@ class PhoneBook {
 	private:
 		Contact _contacts[8];
 		uint8_t	_current_contact;
+		bool	_contacts_filled;
 
 		std::string	_get_input(void);
+		std::string	_trunc(std::string field);
+		void		_print_contact(uint8_t contact_index);
 };
